5fad708f42115a4982894fefc79774eb73ceba65
Wojtysek <vojtahr1@post.cz>
2022-06-20 13:42:34 +0200

    Přidejte soubory projektu.

---
+++ ciferace/ciferace.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int cifra;
+	int soucet = 0;
+	int i;
+	int j;
+	int vysledek = 0;
+	printf("Zadejte cislo pro ciferny soucet: ");
+	scanf_s("%d", &cifra);
+	while (cifra > 0)
+	{
+		i = cifra % 10;
+		soucet = soucet + i;
+		cifra = cifra / 10;
+	}
+	while (soucet > 0) {
+
+		j = soucet % 10;
+		vysledek = vysledek + j;
+		soucet = soucet / 10;
+	}
+	printf("\n");
+	printf("vysledek je %d\n", vysledek);
+}
